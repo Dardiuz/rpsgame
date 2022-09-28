4c0bcfbcd62ec4f18494be0710aac8d257a62816
Dardiuz <Donnynoorlander@gmail.com>
2022-09-28 15:02:35 +0200

    Added: game & working on who wins based on picked option

--- javascript.js
+++ javascript.js
@@ -1,22 +1,43 @@
-console.log("Hello Word!");
-
 //rock paper scissor game player vs computer played in the console
 //game will have 5 rounds (loop)
 //return results
 //compare player + computer input for results
 //win or lose text in the console
-
 //player input by prompt
 //player input validation (spelling & case sensitivity)
 
-//global choices for player & computer variable
-var choices = ["rock", "paper", "scissors"];
-
 //computer choice randomized
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 console.log(getComputerChoice());
 
-function playerInput() {}
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "You tied! You both picked ${playerSelection}";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    compScore++;
+    return "You Lost! Rock destroys scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    compScore++;
+    return "You Lost! Rock destroys scissors";
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt(
+      "Choose wisely: 'Rock', 'Paper' or 'Scissors' "
+    ).toLowerCase();
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+  }
+}
